08c8b88ea231cf673dfb8aacdad97b99ca457a87
harveyspectar <ultimatepravin72@gmail.com>
2017-08-01 18:41:24 +0200

    serrano-common: slim

--- doze/Android.mk
+++ doze/Android.mk
@@ -5,11 +5,7 @@ LOCAL_MODULE_TAGS := optional
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
-<<<<<<< HEAD:doze/Android.mk
 LOCAL_PACKAGE_NAME := SamsungDoze
-=======
-LOCAL_PACKAGE_NAME := LenovoDoze
->>>>>>> 5365ab5... A6020: Add LenovoDoze package:doze/Android.mk
 LOCAL_CERTIFICATE := platform
 LOCAL_PRIVILEGED_MODULE := true
 
